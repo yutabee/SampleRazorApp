e7b0bb39e90d2960ceb2d48b7acc3f2e88d2c135
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-18 18:39:46 +0900

    [modified] #5 フォームとモデルを関連付け

--- SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -1,15 +1,33 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿// using System.ComponentModel.DataAnnotations;  追記する
+
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.ComponentModel.DataAnnotations;
 
-namespace SampleRazorApp.Pages
+public class IndexModel : PageModel
 {
-    public class IndexModel : PageModel
-    {
-        public string Message { get; set; } = "sample message";
+    public string Message = "no message.";
+    /*データの型を指定*/
+    /*フォームと関連付けされたプロパティは送信後もvalueに保持される*/
+    [DataType(DataType.Text)]
+    public string Name { get; set; }
 
-        public void OnGet()
-        {
-            Message = "何か書いて下さい。";
-        }
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [DataType(DataType.EmailAddress)]
+    public string Mail { get; set; }
+
+    [DataType(DataType.PhoneNumber)]
+    public string Tel { get; set; }
+
+    public void OnGet()
+    {
+        Message = "入力して下さい。";
+    }
+
+    /*設定したプロパティと同名の引数を設定すると受け取れる。（小文字でOK！！）*/
+    public void OnPost(string name, string password, string mail, string tel)
+    {
+        Message = $"[Name: {name}, password:({password.Length}chars), mail:{mail}<{tel}>]";
     }
 }
