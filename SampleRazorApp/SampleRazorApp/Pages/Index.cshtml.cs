f28f6cd37fcbbd331cda264d78746396b7807192
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-18 17:48:12 +0900

    [add] #4 フォーム送信の実装 Index.cshtml=>Other.cshtmlに送信する簡単なフォームを実装

--- SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -6,26 +6,10 @@ namespace SampleRazorApp.Pages
     public class IndexModel : PageModel
     {
         public string Message { get; set; } = "sample message";
-        private string[][] data = new string[][] {
-        new string[]{"Taro", "taro@yamada"},
-        new string[]{"Hanako", "hanako@flower"},
-        new string[]{"Sachiko", "sachiko@happy"}
-    };
-
-        //クエリーパラメーターをページモデルにバインドする
-        [BindProperty(SupportsGet = true)]
-        public int id { get; set; }
 
         public void OnGet()
         {
-            Message = "これはMessageプロパティの値です。";
+            Message = "何か書いて下さい。";
         }
-
-        public string getData(int id)
-        {
-            string[] target = data[id];
-            return "[名前：" + target[0] + ", メール：" + target[1] + "]";
-        }
- 
     }
 }
