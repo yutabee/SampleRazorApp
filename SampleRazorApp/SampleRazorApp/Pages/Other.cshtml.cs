36de1898733be903968847630fcedd4dfec42de3
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-18 20:11:58 +0900

    [add] #7 その他フォームサンプル機能の作成 チェックボックス、ラジオボタン、リストを作成

--- SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
@@ -3,15 +3,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
+    public enum Gender
+    {
+        male,
+        female
+    }
+    public enum Platform
+    {
+        Windows,
+        macOS,
+        Linux,
+        ChromeOS,
+        Android,
+        iOS
+    }
+
     public class OtherModel : PageModel
     {
-        public string Message { get; set; }
+        public string? Message { get; set; }
 
-        /*OnPostがPOSTメソッドを受け取った時の処理*/
-        public void OnPost()
+        public bool check { get; set; }
+        public Gender gender { get; set; }
+        public Platform pc { get; set; }
+        public Platform[] pc2 { get; set; }
+
+        public void OnGet()
         {
-            /*クライアントから送信されたフォームの「name="msg"」*/
-            Message = "you typed: " + Request.Form["msg"];
+            Message = "check & select it!";
+        }
+
+        public void OnPost(bool check, string gender, Platform pc, Platform[] pc2)
+        {
+            Message = "Result: " + check + "," + gender + "," + pc + ", " + pc2.Length;
         }
     }
 }
