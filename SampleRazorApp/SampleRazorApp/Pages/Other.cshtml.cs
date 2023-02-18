5ff9d6e537c51191042b16a985d1fb4057ec97e9
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-18 18:04:53 +0900

    [update] #4 フォームから送信された値をOtherPageに反映

--- SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Other.cshtml.cs
@@ -5,8 +5,13 @@ namespace SampleRazorApp.Pages
 {
     public class OtherModel : PageModel
     {
-        public void OnGet()
+        public string Message { get; set; }
+
+        /*OnPostがPOSTメソッドを受け取った時の処理*/
+        public void OnPost()
         {
+            /*クライアントから送信されたフォームの「name="msg"」*/
+            Message = "you typed: " + Request.Form["msg"];
         }
     }
 }
