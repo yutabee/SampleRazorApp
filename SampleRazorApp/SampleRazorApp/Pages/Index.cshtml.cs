588eb2b5981359bd299af1ec73684501abf41f72
Yuta Tamaru <y-tamaru@alterbooth.com>
2023-02-18 15:55:11 +0900

    [modify] #2 ModelClass Uage

--- SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
+++ SampleRazorApp/SampleRazorApp/Pages/Index.cshtml.cs
@@ -3,21 +3,13 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace SampleRazorApp.Pages
 {
-    public class IndexModel : PageModel　
+    public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        public string Message { get; set; } = "sample message";
 
-        public IndexModel(ILogger<IndexModel> logger)　// ILogger=>デバッグ用のログ出力機能を提供
-        {
-            _logger = logger;
-        }
-
-        //RazorPageにアクセスした際に実行される
-        //RazorPageではPageModelとPageFileがセットで用意されるため、
-        //対応するファイルを自動でレンダリングする
         public void OnGet()
         {
-
+            Message = "これはMessageプロパティの値です。";
         }
     }
 }
